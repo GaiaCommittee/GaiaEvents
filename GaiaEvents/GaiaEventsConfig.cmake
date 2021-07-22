25ada6db9973ce32fe354beb3e9f9044901231db
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:40:01 +0800

    Change install script, add version support.

--- GaiaEvents/GaiaEventsConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaEvents_INCLUDE_DIRS "GaiaEvents")
-find_library(GaiaEvents_LIBS "GaiaEvents")
-
-if (GaiaEvents_INCLUDE_DIRS AND GaiaEvents_LIBS)
-    set(GaiaExceptions_FOUND TRUE)
-endif()
